022577aa8be2e1e0fdb3bc046e35675bef6becb8
Délita MAKANDA <delita.makanda@axivit.com>
2026-06-12 15:55:14 +0200

    feat: enhance routing and add new features for dashboard, audits, integrations, notifications, and prioritization

--- orion-web/src/app/app.routes.ts
+++ orion-web/src/app/app.routes.ts
@@ -1,18 +1,46 @@
 import { Routes } from '@angular/router';
+import { AppShellComponent } from '@app/core/layout/app-shell/app-shell.component';
+import { authGuard } from '@app/core/auth/auth.guard';
 
 export const routes: Routes = [
     {
         path: '',
-        loadComponent: () => import('./core/layout/app-shell/app-shell.component').then(m => m.AppShellComponent),
+        component: AppShellComponent,
+        canActivate: [authGuard],
         children: [
-            {
-                path: 'dashboard',
-                loadComponent: () => import('./features/dashboard/pages/dashboard-page/dashboard-page').then(m => m.DashboardPage)
-            },
             { 
-                path: '**', 
+                path: 'dashboard', 
+                loadChildren: () => import('./features/dashboard/dashboard.routes').then(m => m.routes),
+            },
+            {
+                path: 'complaints',
+                loadChildren: () => import('./features/complaints/complaints.routes').then(m => m.routes),
+            },
+            {
+                path: 'prioritization',
+                loadChildren: () => import('./features/prioritization/prioritization.routes').then(m => m.routes),
+            },
+            {
+                path: 'integrations',
+                loadChildren: () => import('./features/integrations/integrations.routes').then(m => m.routes),
+            },
+            {
+                path: 'audits',
+                loadChildren: () => import('./features/audits/audits.routes').then(m => m.routes),
+            },
+            {
+                path: 'notifications',
+                loadChildren: () => import('./features/notifications/notifications.routes').then(m => m.routes),
+            },
+            {
+                path: '**',
+                pathMatch: 'full',
                 redirectTo: 'dashboard',
             }
         ],
     },
+    {
+        path: 'login',
+        loadComponent: () => import('./features/auth/pages/auth-page/auth-page').then(m => m.AuthPage),
+    }
 ];
---
+++ orion-web/src/app/features/integrations/pages/sync-monitoring-page/sync-monitoring-page.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sync-monitoring-page',
+  imports: [],
+  templateUrl: './sync-monitoring-page.html',
+  styleUrl: './sync-monitoring-page.css',
+})
+export class SyncMonitoringPage {}
