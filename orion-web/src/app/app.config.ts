7d28dfced0c9d9165c9d92d3e07f5fbee777621c
Délita MAKANDA <delita.makanda@axivit.com>
2026-06-17 13:51:19 +0200

    feat: add error and loading interceptors to HTTP client configuration

--- orion-web/src/app/app.config.ts
+++ orion-web/src/app/app.config.ts
@@ -1,4 +1,4 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection  } from '@angular/core';
+import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
 import { provideRouter, withComponentInputBinding, withInMemoryScrolling, withViewTransitions } from '@angular/router';
 
 import { routes } from './app.routes';
@@ -7,25 +7,24 @@ import { API_CONFIG_TOKEN } from '@app/core/config/injection-token';
 import { API_CONFIG } from '@app/core/config/api.config';
 import { authInterceptor } from '@app/core/api/auth.interceptor';
 import { MATERIAL_PROVIDERS } from './shared/material.imports';
+import { loadingInterceptor } from './core/api/loading.interceptor';
+import { errorInterceptor } from './core/api/error.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
-    provideHttpClient(
-       withInterceptors([authInterceptor])
-    ),
+    provideHttpClient(withInterceptors([authInterceptor, errorInterceptor, loadingInterceptor])),
     {
       provide: API_CONFIG_TOKEN,
-      useValue: API_CONFIG
+      useValue: API_CONFIG,
     },
     ...MATERIAL_PROVIDERS,
     provideRouter(
       routes,
       withViewTransitions(),
       withComponentInputBinding(),
-      withInMemoryScrolling({ scrollPositionRestoration: 'enabled', anchorScrolling: 'enabled' })
+      withInMemoryScrolling({ scrollPositionRestoration: 'enabled', anchorScrolling: 'enabled' }),
     ),
-    
-  ]
+  ],
 };
