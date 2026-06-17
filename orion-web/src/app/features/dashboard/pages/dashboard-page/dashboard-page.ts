599591a6e2f67664f9bd81b68a8087d9ae35e22e
Délita MAKANDA <delita.makanda@axivit.com>
2026-06-17 17:31:36 +0200

    feat: integrate shared UI components into dashboard page

--- orion-web/src/app/features/dashboard/pages/dashboard-page/dashboard-page.ts
+++ orion-web/src/app/features/dashboard/pages/dashboard-page/dashboard-page.ts
@@ -1,15 +1,12 @@
 import { Component, inject } from '@angular/core';
 import { DashboardStore } from '../../data-access/dashboard.store';
 import { JsonPipe } from '@angular/common';
+import { SHARED_UI_COMPONENTS } from '@app/shared/ui/components.module';
 
 @Component({
   selector: 'app-dashboard-page',
-  imports: [
-    JsonPipe,
-  ],
-  providers: [
-    DashboardStore,
-  ],
+  imports: [JsonPipe, ...SHARED_UI_COMPONENTS],
+  providers: [DashboardStore],
   standalone: true,
   templateUrl: './dashboard-page.html',
   styleUrls: ['./dashboard-page.css'],
