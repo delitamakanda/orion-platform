b092e9f4dd2a4ef22dbf07ae6274c3d58d16d756
Délita MAKANDA <delita.makanda@axivit.com>
2026-06-18 11:28:04 +0200

    feat: update dashboard page and improved loading/error handling

--- orion-web/src/app/features/dashboard/pages/dashboard-page/dashboard-page.ts
+++ orion-web/src/app/features/dashboard/pages/dashboard-page/dashboard-page.ts
@@ -1,17 +1,19 @@
-import { Component, inject } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, inject } from '@angular/core';
 import { DashboardStore } from '../../data-access/dashboard.store';
-import { DatePipe, JsonPipe } from '@angular/common';
+import { DatePipe } from '@angular/common';
 import { SHARED_UI_COMPONENTS } from '@app/shared/ui/components.module';
 import { MATERIAL_IMPORTS } from '@app/shared/material.imports';
 
 @Component({
   selector: 'app-dashboard-page',
-  imports: [JsonPipe, ...SHARED_UI_COMPONENTS, ...MATERIAL_IMPORTS, DatePipe],
+  imports: [...SHARED_UI_COMPONENTS, ...MATERIAL_IMPORTS, DatePipe],
   providers: [DashboardStore],
-  standalone: true,
   templateUrl: './dashboard-page.html',
   styleUrls: ['./dashboard-page.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class DashboardPage {
   readonly store = inject(DashboardStore);
+
+  readonly data = computed(() => this.store.kpis() ?? this.store.dashboards());
 }
