c6efe6ecdd224fe99773b4c2319b029eb54219ec
Délita MAKANDA <delita.makanda@axivit.com>
2026-06-18 17:36:17 +0200

    feat: refactor complaint card component to use Angular Material and enhance layout

--- orion-web/src/app/features/complaints/components/complaint-card/complaint-card.ts
+++ orion-web/src/app/features/complaints/components/complaint-card/complaint-card.ts
@@ -1,12 +1,19 @@
 import { Component, input } from '@angular/core';
 import { Complaint } from '../../models/complaint.model';
+import { MATERIAL_IMPORTS } from '@app/shared/material.imports';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-complaint-card',
-  imports: [],
+  imports: [MATERIAL_IMPORTS, DatePipe],
   templateUrl: './complaint-card.html',
   styleUrls: ['./complaint-card.css'],
 })
 export class ComplaintCard {
   complaint = input.required<Complaint>();
+
+  onSelectComplaint(): void {
+    // Logic to handle complaint selection, e.g., navigate to a detail page or emit an event
+    console.log('Selected complaint:', this.complaint());
+  }
 }
