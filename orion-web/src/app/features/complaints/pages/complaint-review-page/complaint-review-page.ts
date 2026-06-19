b554a035758cb66fe22ed73a30f0c3d3c5ecb888
Délita MAKANDA <delita.makanda@axivit.com>
2026-06-19 17:57:47 +0200

    feat: remove complaint review page component and associated files

--- orion-web/src/app/features/complaints/pages/complaint-review-page/complaint-review-page.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-complaint-review-page',
-  imports: [],
-  templateUrl: './complaint-review-page.html',
-  styleUrl: './complaint-review-page.css',
-})
-export class ComplaintReviewPage {}
