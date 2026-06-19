b1da97038aca9caf9d09786e106f866ff839547d
Délita MAKANDA <delita.makanda@axivit.com>
2026-06-19 19:58:05 +0200

    refactor: change notification_id type to string in URL pattern

--- orion-api/apps/notifications/urls.py
+++ orion-api/apps/notifications/urls.py
@@ -6,9 +6,11 @@ from apps.notifications.views import (
 )
 from django.urls import path
 
+app_name = 'notifications'
+
 urlpatterns = [
     path('', GetNotificationsAPIView.as_view(), name='get-notifications'),
-    path('mark-as-read/<int:notification_id>/', MarkNotificationAsReadAPIView.as_view(), name='mark-notification-as-read'),
+    path('mark-as-read/<str:notification_id>/', MarkNotificationAsReadAPIView.as_view(), name='mark-notification-as-read'),
     path('mark-all-as-read/', MarkAllNotificationsAsReadAPIView.as_view(), name='mark-all-notifications-as-read'),
     path('unread-count/', UnreadNotificationsCountAPIView.as_view(), name='unread-notifications-count'),
 ]
