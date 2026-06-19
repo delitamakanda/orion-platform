de36fc536ff1ad2a1afa59aed155bc0b98afbd31
Délita MAKANDA <delita.makanda@axivit.com>
2026-06-19 16:09:35 +0200

    feat: add UnauthorizedPage component and routing for unauthorized access

---
+++ orion-web/src/app/features/unauthorized/pages/unauthorized-page/unauthorized-page.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-unauthorized-page',
+  imports: [],
+  templateUrl: './unauthorized-page.html',
+  styleUrl: './unauthorized-page.css',
+})
+export class UnauthorizedPage {}
