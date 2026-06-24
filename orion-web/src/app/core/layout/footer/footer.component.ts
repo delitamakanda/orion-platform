6d7cc7610e2eeace6c3563b1ae9a9e41b3d28090
Délita MAKANDA <delita.makanda@axivit.com>
2026-06-24 18:08:05 +0200

    refactor: update FooterComponent to use computed for currentYear and manage date format options

--- orion-web/src/app/core/layout/footer/footer.component.ts
+++ orion-web/src/app/core/layout/footer/footer.component.ts
@@ -1,11 +1,18 @@
-import { Component, signal } from '@angular/core';
+import { DATE_PIPE_DEFAULT_OPTIONS } from '@angular/common';
+import { Component, computed } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
   imports: [],
+  providers: [
+    {
+      provide: DATE_PIPE_DEFAULT_OPTIONS,
+      useValue: { dateFormat: 'shortDate' },
+    },
+  ],
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.css'],
 })
 export class FooterComponent {
-  readonly currentYear = signal(new Date().getFullYear());
+  readonly currentYear = computed(() => new Date().getFullYear());
 }
