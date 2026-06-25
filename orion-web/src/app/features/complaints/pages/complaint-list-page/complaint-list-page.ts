b2fe3655061b6d628a64c802142e2adc54969cd5
Délita MAKANDA <delita.makanda@axivit.com>
2026-06-25 13:53:46 +0200

    refactor: enhance complaint list page with table display and loading spinner

--- orion-web/src/app/features/complaints/pages/complaint-list-page/complaint-list-page.ts
+++ orion-web/src/app/features/complaints/pages/complaint-list-page/complaint-list-page.ts
@@ -1,21 +1,54 @@
-import { Component, inject } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, effect } from '@angular/core';
+import { ScrollingModule } from '@angular/cdk/scrolling';
+import { DatePipe } from '@angular/common';
+import { MatIconModule } from '@angular/material/icon';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { ComplaintStore } from '../../data-access/complaint.store';
-import { MATERIAL_IMPORTS } from '@app/shared/material.imports';
 import { ComplaintFilters } from '../../components/complaint-filters/complaint-filters';
-import { ComplaintCard } from '../../components/complaint-card/complaint-card';
 import { ComplaintFilter } from '../../models/complaint-filters.model';
+import { Complaint } from '../../models/complaint.model';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { A11yModule } from '@angular/cdk/a11y';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-complaint-list-page',
-  imports: [MATERIAL_IMPORTS, ComplaintCard, ComplaintFilters],
-  providers: [ComplaintStore],
+  imports: [
+    ComplaintFilters,
+    ScrollingModule,
+    DatePipe,
+    MatIconModule,
+    MatTableModule,
+    MatProgressSpinnerModule,
+    A11yModule,
+    RouterLink,
+  ],
   templateUrl: './complaint-list-page.html',
   styleUrls: ['./complaint-list-page.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ComplaintListPage {
   readonly store = inject(ComplaintStore);
 
-  updateFilters(filter: Partial<ComplaintFilter>) {
+  displayedColumns: string[] = [
+    'id',
+    'reference',
+    'title',
+    'status',
+    'category',
+    'location',
+    'incident_date',
+    'vulnerability_victim',
+  ];
+  dataSource = new MatTableDataSource<Complaint>([]);
+
+  constructor() {
+    effect(() => {
+      this.dataSource.data = this.store.complaints();
+    });
+  }
+
+  updateFilters(filter: Partial<ComplaintFilter>): void {
     this.store.updateFilters(filter);
   }
 }
