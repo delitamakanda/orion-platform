c32b1a7d0259365d7b45b7ac638c90eb35898e36
Délita MAKANDA <delita.makanda@axivit.com>
2026-06-25 18:59:40 +0200

    feat: refactor priority badge component to use MatChip and improve badge color logic

--- orion-web/src/app/shared/ui/priority-badge/priority-badge.ts
+++ orion-web/src/app/shared/ui/priority-badge/priority-badge.ts
@@ -1,13 +1,41 @@
 import { TitleCasePipe } from '@angular/common';
-import { Component, input } from '@angular/core';
+import { Component, input, computed } from '@angular/core';
+import { MatChipsModule } from '@angular/material/chips';
 import { PriorityLevel } from '@app/features/prioritization/models/priority-level.model';
+import { MatTreeModule } from '@angular/material/tree';
 
 @Component({
   selector: 'app-priority-badge',
-  imports: [TitleCasePipe],
+  imports: [MatChipsModule, TitleCasePipe, MatTreeModule],
   templateUrl: './priority-badge.html',
   styleUrls: ['./priority-badge.css'],
 })
 export class PriorityBadge {
-  readonly priority = input<PriorityLevel>();
+  priority = input<PriorityLevel>();
+
+  readonly badgeColor = computed(() => {
+    switch (this.priority()) {
+      case 'low':
+        return 'primary';
+      case 'medium':
+        return 'accent';
+      case 'high':
+        return 'warn';
+      default:
+        return 'danger';
+    }
+  });
+
+  readonly badgeText = computed(() => {
+    switch (this.priority()) {
+      case 'low':
+        return 'Faible';
+      case 'medium':
+        return 'Moyenne';
+      case 'high':
+        return 'Élevée';
+      default:
+        return 'Critique';
+    }
+  });
 }
