0e150b1a155a90eaf5c4662b8fbf98907cd9d0d6
Délita MAKANDA <delita.makanda@axivit.com>
2026-06-26 15:32:21 +0200

    feat: enhance audits page with table display and loading/error handling

--- orion-web/src/app/features/audits/pages/audits-page/audits-page.ts
+++ orion-web/src/app/features/audits/pages/audits-page/audits-page.ts
@@ -1,12 +1,39 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, effect, ChangeDetectionStrategy } from '@angular/core';
 import { AuditStore } from '../../data-access/audit.store';
+import { ScrollingModule } from '@angular/cdk/scrolling';
+import { DatePipe } from '@angular/common';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { A11yModule } from '@angular/cdk/a11y';
+import { Audit } from '../../models/audit.model';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 @Component({
   selector: 'app-audits-page',
-  providers: [AuditStore],
   templateUrl: './audits-page.html',
+  imports: [
+    ScrollingModule,
+    DatePipe,
+    MatIconModule,
+    MatTableModule,
+    MatProgressSpinnerModule,
+    A11yModule,
+    MatTooltipModule,
+  ],
   styleUrls: ['./audits-page.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AuditsPage {
   readonly store = inject(AuditStore);
+
+  displayedColumns: string[] = ['id', 'user_username', 'ip_address', 'action', 'timestamp', 'action_details'];
+
+  dataSource = new MatTableDataSource<Audit>([]);
+
+  constructor() {
+    effect(() => {
+      this.dataSource.data = this.store.audits();
+    });
+  }
 }
