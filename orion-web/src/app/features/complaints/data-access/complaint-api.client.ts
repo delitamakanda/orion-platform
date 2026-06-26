c9ba25abea36322150474dbb4fbff2b41188b571
Délita MAKANDA <delita.makanda@axivit.com>
2026-06-26 16:14:05 +0200

    feat: add loading interceptor context to complaint API calls

--- orion-web/src/app/features/complaints/data-access/complaint-api.client.ts
+++ orion-web/src/app/features/complaints/data-access/complaint-api.client.ts
@@ -1,9 +1,10 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpContext } from '@angular/common/http';
 import { inject, Service } from '@angular/core';
 import { API_CONFIG_TOKEN } from '@app/core/config/injection-token';
 import { map } from 'rxjs';
 import { Complaint } from '../models/complaint.model';
 import { ComplaintFilter } from '@app/features/complaints/models/complaint-filters.model';
+import { SKIP_LOADING_INTERCEPTOR } from '@app/core/api/loading.interceptor';
 
 @Service()
 export class ComplaintApiClient {
@@ -15,13 +16,18 @@ export class ComplaintApiClient {
     return this.http
       .get<{
         data: { results: Complaint[]; next: string; previous: string };
-      }>(`${this.config.backendUrl}/complaints/`, { params })
+      }>(`${this.config.backendUrl}/complaints/`, {
+        params,
+        context: new HttpContext().set(SKIP_LOADING_INTERCEPTOR, true),
+      })
       .pipe(map((response) => response.data.results || []));
   }
 
   findOne(id: number) {
     return this.http
-      .get<{ data: Complaint }>(`${this.config.backendUrl}/complaints/${id}/`)
+      .get<{
+        data: Complaint;
+      }>(`${this.config.backendUrl}/complaints/${id}/`, { context: new HttpContext().set(SKIP_LOADING_INTERCEPTOR, true) })
       .pipe(map((complaint) => complaint['data'] || complaint));
   }
 }
